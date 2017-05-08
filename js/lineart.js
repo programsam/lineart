ee6940a22602de2e14772e301b187f7ab8feb205
Ben Smith <programsam@gmail.com>
2017-05-08 12:55:55 -0400

    Lots of dem features.

--- js/lineart.js
+++ js/lineart.js
@@ -3,7 +3,8 @@ var mytop = 0;
 var maxwidth = 0;
 var maxheight = 0;
 var canvas, clock;
-var previousDirection = -1;
+var direction;
+var circle;
 
 $(document).ready(function(){
 	
@@ -17,17 +18,16 @@ $(document).ready(function(){
 	
 	left = maxwidth / 2;
 	mytop = maxheight / 2;
+	
+	circle = new fabric.Circle({left:left, top:mytop, radius: 3})
+	canvas.add(circle)
 
-	clock = setInterval(nextLine, 0);
 	$('#stop').click(stopDrawing)
 	$('#clear').click(clearDrawing)
 	$('#start').click(startDrawing)
 	
-	$('#ex1').slider({
-		formatter: function(value) {
-			return 'Current value: ' + value;
-		}
-	});
+	$('#speed').slider({reversed: true});
+	$('#length').slider({});
 });
 
 function clearDrawing(e) {
@@ -42,37 +42,84 @@ function stopDrawing(e) {
 function startDrawing(e) {
 	if (clock == null)
 	{
-		clock = setInterval(nextLine, 0)
+		clock = setInterval(nextLine, $("#speed")[0].value)
 	}
 }
 
 function nextLine() {
 	
-	direction = previousDirection;
-	while (direction == previousDirection)
+	var doublebacks = $("#doublebacks")[0].checked
+	var repeats = $("#repeats")[0].checked
+
+	var repeatsOK = false;
+	var doublebacksOK = false;
+	var previousDirection = direction;
+	var length = 0;
+	while (! repeatsOK || ! doublebacksOK || ! endOK)
 	{
+		repeatsOK = false;
+		doublebacksOK = false;
+		endOK = false;
+		
 		direction = randomWithRange(0,4)
+		length = randomWithRange(0,$("#length")[0].value)
+		
+		if (repeats)
+		{
+			repeatsOK = true;
+		}
+		else //repeats not allowed
+		{
+			if (direction != previousDirection)
+				repeatsOK = true
+		}
+		
+		if (doublebacks)
+		{
+			doublebacksOK = true;
+		}
+		else
+		{
+			doublebacksOK = true;
+			if (direction == 0 && previousDirection == 2)
+				doublebacksOK = false;
+			if (direction == 2 && previousDirection == 0)
+				doublebacksOK = false;
+			if (direction == 1 && previousDirection == 3)
+				doublebacksOK = false;
+			if (direction == 3 && previousDirection == 1)
+				doublebacksOK = false;
+		}
+		
+		if ((direction == 0) && (left + length < maxwidth))
+			endOK = true;
+		else if ((direction == 1) && (mytop + length < maxheight))
+			endOK = true;
+		else if ((direction == 2) && (left - length > 0))
+			endOK = true;
+		else if ((direction == 3) && (mytop - length > 0))
+			endOK = true;
 	}
-	var length = randomWithRange(0,80)
+
 	var x = 0;
 	var y = 0;
 	
-	if ((direction == 0) && (left + length < maxwidth)) //left to right
+	if (direction == 0) //left to right
 	{
 		x = left + length;
 		y = mytop;
 	}
-	else if ((direction == 1) && (mytop + length < maxheight)) //top to bottom
+	else if (direction == 1) //top to bottom
 	{
 		x = left;
 		y = mytop + length;
 	}
-	else if ((direction == 2) && (left - length > 0)) //right to left
+	else if (direction == 2) //right to left
 	{
 		x = left - length;
 		y = mytop;
 	}
-	else if ((direction == 3) && (mytop - length > 0)) //left to right
+	else if (direction == 3) //bottom to top
 	{
 		x = left;
 		y = mytop - length;
@@ -107,6 +154,11 @@ function nextLine() {
 	{
 		mytop = mytop - length;
 	}
+	
+	circle.setLeft(left);
+	circle.setTop(mytop);
+	
+	canvas.renderAll();
 }
 
 
